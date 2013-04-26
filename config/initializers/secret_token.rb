f7b5753d635c1dfdbeb85b7b94dd8a3551e35531
Jonathan Chhabra <fresh@freshemail.com>
2013-04-25 22:08:56 -0400

    First commit
    created the app and modified the ignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5a478f717851b9376e1ee26424c5e0729b005025271f965a1dccb4c7268ae196c276d12e82059930a655d7548d8192ceee66422a24ad97568cee93c406958ee2'
